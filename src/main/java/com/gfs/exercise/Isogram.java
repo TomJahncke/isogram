c57b49f7539eaffc454780a8c0925721153de4bc
Brentt Smith <brentt.smith@gfs.com>
2021-01-04 20:42:29 -0500

    Initial Setup

---
+++ src/main/java/com/gfs/exercise/Isogram.java
@@ -0,0 +1,29 @@
+package com.gfs.exercise;
+
+/**
+ * An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that
+ * determines whether a string that contains only letters is an isogram.
+ * <p>
+ * <p>
+ * 1. Assume the empty string is an isogram.
+ * 2. Assume null is not an isogram.
+ * 3. Ignore letter case.
+ * 4. A String containing any character other than a letter should throw an IllegalArgumentException
+ * <p>
+ * <p>
+ * Please use Test Driven Development style to show how you would prove your code works.
+ * <p>
+ * <p>
+ * isIsogram "Dermatoglyphics" == true
+ * isIsogram "aba" == false
+ * isIsogram "moOse" == false -- ignore letter case
+ *
+ * @param word
+ * @return boolean - true if a word is an isogram
+ */
+class Isogram {
+
+    public boolean determineIsAnIsogram(String word) {
+        return false;
+    }
+}
